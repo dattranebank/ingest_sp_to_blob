2c0b3be04a99a01be88730107131c627a29c40cc
Dat Tran Ebank <ebankubu111444@gmail.com>
2025-10-04 23:07:25 +0700

    HTTP Trigger

--- function_app.py
+++ function_app.py
@@ -9,9 +9,9 @@ from azure.core.exceptions import ResourceExistsError
 app = func.FunctionApp()
 
 
-# Timer Trigger cập nhật (giờ UTC)
-# Chạy lúc 3g UTC = 10g VN, 13g UTC = 20g VN
-@app.schedule(schedule="0 0 3,13 * * *", arg_name="myTimer", run_on_startup=True)
+# HTTP Trigger: gọi thủ công hoặc từ ADF
+@app.function_name(name="copy_sp_to_blob")
+@app.route(route="copy_sp_to_blob", methods=["POST"])
 def copy_sp_to_blob(myTimer: func.TimerRequest) -> None:
 
     # Auth setup
